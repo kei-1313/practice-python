d272949ec11d7369b3a590248fa9025ff209190e
kei-1313 <candydog666@gmail.com>
2026-01-02 16:43:39 +0900

    add async functions for concurrent execution with asyncio

---
+++ async/async.py
@@ -0,0 +1,26 @@
+import asyncio
+import time
+
+async def func_1(sec):
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, time.sleep, sec)
+    return "完了func1"
+
+async def func_2(sec):
+    await asyncio.sleep(sec)
+    return "完了func2"
+
+
+async def main():
+    print(f"開始: {time.strftime('%X')}")
+    task1 = asyncio.create_task(func_1(1))
+    task2 = asyncio.create_task(func_2(2))
+    await task1
+    await task2
+
+    print(task1.result())
+    print(task2.result())
+    print(f"終了: {time.strftime('%X')}")
+
+if __name__ == '__main__':
+    asyncio.run(main())
